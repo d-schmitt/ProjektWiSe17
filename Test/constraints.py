518e3c3c3b5d57033f65ab2251c1a306a835269b
d-schmitt <daniela.schmitt@mailbox.org>
2017-12-19 13:34:45 +0100

    Add files via upload

--- Test/constraints.py
+++ Test/constraints.py
@@ -1,6 +1,6 @@
-
 from Test.reschedulingHelpers import *
 from datetime import datetime, timedelta, date
+
 
 def checkShiftAssignments(roster):
     """
@@ -12,29 +12,30 @@ def checkShiftAssignments(roster):
 
     # for each employee i
     for i in roster.employees:
-        ename = i.fName+" "+i.lName
+        ename = i.fName + " " + i.lName
         onList, offList = roster.getOnOffDays(ename)
 
         # for each day he/she neither has an offday or vacation day
         for j in onList:
-            shiftassignments = 0 # counts how many shifts have been assigned to one employee per day
+            shiftassignments = 0  # counts how many shifts have been assigned to one employee per day
 
             # for each shift
             for k in roster.shiftTypes:
 
-            # check if employee i works shift k on day j
+                # check if employee i works shift k on day j
                 # make a list with working employees for the day and shift
                 EmployeesWorkingShiftThatDay = roster.getWorkingEmployees(j, k['name'])
 
                 # check if current employee i is in list
                 for e in EmployeesWorkingShiftThatDay:
-                    if(e ==ename):
-                        shiftassignments = shiftassignments+1
+                    if (e == ename):
+                        shiftassignments = shiftassignments + 1
 
             # if one employee has more than one shift per day assigned, return false
-            if(shiftassignments>1):
+            if (shiftassignments > 1):
                 return False
     return True
+
 
 def checkDaysOff_old(roster):
     """
@@ -73,6 +74,7 @@ def checkDaysOff_old(roster):
 
     return True
 
+
 def checkVacationDaysOff(roster):
     """
     Each employee should not work on his/her day offs.
@@ -83,11 +85,12 @@ def checkVacationDaysOff(roster):
     # Für jeden Mitarbeiter
     for i in roster.employees:
         # for each vacation day
-        if(len(i.vacation)>0): # da Leihnurse keine Vacation Days hat
-            for j in i.vacation: # Für jeden Vacation Day
-                if(getShiftByEmployeeByDate01(roster,j,i)==1): # Wenn Nurse dort arbeitet, dann return false
+        if (len(i.vacation) > 0):  # da Leihnurse keine Vacation Days hat
+            for j in i.vacation:  # Für jeden Vacation Day
+                if (getShiftByEmployeeByDate01(roster, j, i) == 1):  # Wenn Nurse dort arbeitet, dann return false
                     return False
     return True
+
 
 def checkOffDaysOff(roster):
     """
@@ -99,11 +102,12 @@ def checkOffDaysOff(roster):
     # Für jeden Mitarbeiter
     for i in roster.employees:
         # for each Off day
-        if(len(i.off)>0): # da Leihnurse keine Off Days hat
-            for j in i.off: # Für jeden Off Day
-                if(getShiftByEmployeeByDate01(roster,j,i)==1): # Wenn Nurse dort arbeitet, dann return false
+        if (len(i.off) > 0):  # da Leihnurse keine Off Days hat
+            for j in i.off:  # Für jeden Off Day
+                if (getShiftByEmployeeByDate01(roster, j, i) == 1):  # Wenn Nurse dort arbeitet, dann return false
                     return False
     return True
+
 
 def checkShiftAssignmentToDemand(roster):
     """
@@ -146,6 +150,7 @@ def checkShiftAssignmentToDemand(roster):
             return False
     return True
 
+
 def checkWeeklyWorkingHours(roster):
     """
     Für jeden Employee und jede Woche gilt:
@@ -165,6 +170,7 @@ Für jede Schicht s
 
 Wenn getContract(i).RegularWorkingHours != """
 
+
 def checkMinMaxConsec(roster):
     """
     Constraint 7+8+9 for Minimum and Maximum of consecutive working days
@@ -179,13 +185,13 @@ def checkMinMaxConsec(roster):
     end_date = date(end_period.year, end_period.month, end_period.day)
 
     # for each day
-    for single_date in daterange(start_date, end_date):# checkt er hier auch letzten Tag?
+    for single_date in daterange(start_date, end_date):  # checkt er hier auch letzten Tag?
         sdDay = single_date.strftime("%Y-%m-%d")
-        if(sdDay != roster.start): # for all days not being the first day in the period
+        if (sdDay != roster.start):  # for all days not being the first day in the period
             sdDayBefore = (single_date - timedelta(days=1)).strftime("%Y-%m-%d")
             # for each employee
             for i in roster.employees:
-                if (i.lName != "Nurse"): # for all nurses except Leih Nurses
+                if (i.lName != "Nurse"):  # for all nurses except Leih Nurses
                     startDay = getConsecStartDay(sdDay, roster, i)
                     eWorks = getShiftByEmployeeByDate01(roster, sdDay, i)
                     eWorksY = getShiftByEmployeeByDate01(roster, sdDayBefore, i)
@@ -194,20 +200,21 @@ def checkMinMaxConsec(roster):
                         or (eWorks - startDay < 0)
                         or (startDay + eWorksY > 1)):
                         return False
-        else: # for first day in period
+        else:  # for first day in period
             # for each employee
             for i in roster.employees:
-                if(i.lName != "Nurse"):
+                if (i.lName != "Nurse"):
                     startDay = getConsecStartDay(sdDay, roster, i)
-                    oldPeriod=0
-                    if(i.history["lastAssignedShiftType"]=="Spaet" or i.history["lastAssignedShiftType"]=="Frueh"):
-                        oldPeriod=1
+                    oldPeriod = 0
+                    if (i.history["lastAssignedShiftType"] == "Spaet" or i.history["lastAssignedShiftType"] == "Frueh"):
+                        oldPeriod = 1
                     eWorks = getShiftByEmployeeByDate01(roster, sdDay, i)
                     if ((eWorks - oldPeriod - startDay > 0)
                         or (eWorks - startDay < 0)
                         or (startDay + oldPeriod > 1)):
                         return False
     return True
+
 
 def forbiddenPatterns(roster):
     """
@@ -217,33 +224,33 @@ def forbiddenPatterns(roster):
     currentTime = datetime.strptime(roster.start, '%Y-%m-%d')
 
     start_date = date(currentTime.year, currentTime.month, currentTime.day)
-    end_period = currentTime + timedelta(days=roster.cntDays-1)
+    end_period = currentTime + timedelta(days=roster.cntDays - 1)
     end_date = date(end_period.year, end_period.month, end_period.day)
 
-
-    for f in roster.forbidden: # für jede verbotene Schichtreihenfolge
-        for single_date in daterange(start_date, end_date): # for each day
+    for f in roster.forbidden:  # für jede verbotene Schichtreihenfolge
+        for single_date in daterange(start_date, end_date):  # for each day
             sdDay = single_date.strftime("%Y-%m-%d")
             sdDayTomorrow = (single_date + timedelta(days=1)).strftime("%Y-%m-%d")
-            for i in roster.employees: # for each employee
+            for i in roster.employees:  # for each employee
                 if (i.lName != "Nurse"):  # for all nurses except Leih Nurses
-                    eWorks = getShiftByEmployeeByDateByShift(roster, sdDay, i,f['preceedingShiftType'])
+                    eWorks = getShiftByEmployeeByDateByShift(roster, sdDay, i, f['preceedingShiftType'])
                     eWorksT = getShiftByEmployeeByDateByShift(roster, sdDayTomorrow, i, f['succeedingShiftType'])
-                    if (eWorks + eWorksT >1): # wenn nurse verbotene schichtreihenfolge arbeitet
+                    if (eWorks + eWorksT > 1):  # wenn nurse verbotene schichtreihenfolge arbeitet
                         return False
 
         # Für Periodenübergang (letzter Tag vorheriger Periode und erster Tag dieser Periode)
         sdDay = start_date.strftime("%Y-%m-%d")
         for i in roster.employees:
-            if(i.lName != "Nurse"):
-                eWorks = getShiftByEmployeeByDateByShift(roster,sdDay,i,f['succeedingShiftType'])
+            if (i.lName != "Nurse"):
+                eWorks = getShiftByEmployeeByDateByShift(roster, sdDay, i, f['succeedingShiftType'])
                 eWorksY = 0
-                if(i.history["lastAssignedShiftType"]== f['preceedingShiftType']):
+                if (i.history["lastAssignedShiftType"] == f['preceedingShiftType']):
                     eWorksY = 1
-                if(eWorks +eWorksY > 1):
+                if (eWorks + eWorksY > 1):
                     return False
 
     return True
+
 
 def maxSundays(roster):
     """
@@ -255,27 +262,28 @@ def maxSundays(roster):
 
     currentTime = datetime.strptime(roster.start, '%Y-%m-%d')
     start_date = date(currentTime.year, currentTime.month, currentTime.day)
-    end_period = currentTime + timedelta(days=roster.cntDays-1)
+    end_period = currentTime + timedelta(days=roster.cntDays - 1)
     end_date = date(end_period.year, end_period.month, end_period.day)
 
     periodSun = []
     # erstelle liste mit tagen der sonntage aus periode
-    for single_date in daterange(start_date, end_date+timedelta(days=1)): # für alle Tage der Periode
-        if(single_date.weekday() == 6): # 6 = Sunday
+    for single_date in daterange(start_date, end_date + timedelta(days=1)):  # für alle Tage der Periode
+        if (single_date.weekday() == 6):  # 6 = Sunday
             periodSun.append(single_date.strftime("%Y-%m-%d"))
 
     # überprüfe für jeden tag, ob mitarbeiter an sonntag arbeitet, falls ja, erhöhe workSun
-    if(len(periodSun)>0):
+    if (len(periodSun) > 0):
         for i in roster.employees:
-            if(i.lName != "Nurse"):
+            if (i.lName != "Nurse"):
                 workSun = 0
                 for j in periodSun:
                     eWorks = getShiftByEmployeeByDate01(roster, j, i)
-                    if(eWorks == 1):
+                    if (eWorks == 1):
                         workSun += 1
-                if(workSun > roster.maxSun):
+                if (workSun > roster.maxSun):
                     return False
     return True
+
 
 def weekendDays(roster):
     """
@@ -287,28 +295,28 @@ def weekendDays(roster):
 
     currentTime = datetime.strptime(roster.start, '%Y-%m-%d')
     start_date = date(currentTime.year, currentTime.month, currentTime.day)
-    end_period = currentTime + timedelta(days=roster.cntDays-1)
+    end_period = currentTime + timedelta(days=roster.cntDays - 1)
     end_date = date(end_period.year, end_period.month, end_period.day)
 
     periodSun = []
     # erstelle liste mit tagen der sonntage aus periode
-    for single_date in daterange(start_date, end_date+timedelta(days=1)): # für alle Tage der Periode
-        if(single_date.weekday() == 6): # 6 = Sunday
+    for single_date in daterange(start_date, end_date + timedelta(days=1)):  # für alle Tage der Periode
+        if (single_date.weekday() == 6):  # 6 = Sunday
             periodSun.append(single_date.strftime("%Y-%m-%d"))
 
-
-    if(len(periodSun)>0):
+    if (len(periodSun) > 0):
         for i in roster.employees:
-            if(i.lName != "Nurse"):
+            if (i.lName != "Nurse"):
                 for j in range(len(periodSun)):
-                    eWorks = getShiftByEmployeeByDate01(roster,periodSun[j], i)
+                    eWorks = getShiftByEmployeeByDate01(roster, periodSun[j], i)
                     saturday = (datetime.strptime(periodSun[j], "%Y-%m-%d") - timedelta(days=1)).strftime("%Y-%m-%d")
                     eWorksY = getShiftByEmployeeByDate01(roster, saturday, i)
                     weekendConstraint = i.weekendContraints[j]
-                    if(((eWorksY + eWorks) < 2*eWorks*weekendConstraint)
-                       or ((eWorksY + eWorks) < 2*eWorksY*weekendConstraint)):
+                    if (((eWorksY + eWorks) < 2 * eWorks * weekendConstraint)
+                        or ((eWorksY + eWorks) < 2 * eWorksY * weekendConstraint)):
                         return False
     return True
+
 
 def extraHours(roster):
     """
@@ -324,10 +332,11 @@ def extraHours(roster):
     for i in roster.employees:
         if (i.lName != "Nurse"):
             empExtraHours = i.extraHours
-            if(i.eMaxExtra < empExtraHours or i.minExtra > empExtraHours
-               or roster.maxOver< empExtraHours or roster.maxUnder > empExtraHours):
+            if (i.eMaxExtra < empExtraHours or i.minExtra > empExtraHours
+                or roster.maxOver < empExtraHours or roster.maxUnder > empExtraHours):
                 return False
     return True
+
 
 def fixedAssignments(roster):
     """
@@ -340,9 +349,170 @@ def fixedAssignments(roster):
         employee = int(i['employee'])
         day = i['day']
         shift = i['shiftType']
-        eWorks = getShiftByEmployeeByDateByShift(roster,day,getEmployeeById(roster,employee),shift)
+        eWorks = getShiftByEmployeeByDateByShift(roster, day, getEmployeeById(roster, employee), shift)
         eShift = i.shifts['day']
-        if(eWorks == 0 and
-               (eShift != 'sickDay' and eShift != 'vacationDay' and eShift != 'offDay')):
+        if (eWorks == 0 and eShift != 'sickDay' and eShift != 'vacationDay' and eShift != 'offDay'):
             return False
+    return True
+
+
+def minConsec(roster):
+    """
+    Constraint 10
+    :param roster:
+    :return:
+    """
+    currentTime = datetime.strptime(roster.start, '%Y-%m-%d')
+    start_date = date(currentTime.year, currentTime.month, currentTime.day)
+    end_period = currentTime + timedelta(days=roster.cntDays - 1)
+    end_date = date(end_period.year, end_period.month, end_period.day)
+
+    dayCount = 1  # TODO: startet Day bei 0 oder bei 1?
+    for single_date in daterange(start_date, end_date + timedelta(days=1)):  # für alle Tage der Periode
+        for i in roster.employees:  # für alle Mitarbeiter
+
+            # Berechnung wieviele Tage in bisheriger Periode gearbeitet wurden
+            daySum = min(roster.cntDays, (dayCount + i.minConsecutive - 1))
+            consecDays = 0
+            for j in range(dayCount, daySum + 1):
+                d = single_date + timedelta(days=j - 1)
+                d = d.strftime("%Y-%m-%d")
+                consecDays += getShiftByEmployeeByDate01(roster, d, i)
+
+            consecutiveStart = consecStart(roster, single_date, i)
+            minDays = min(i.minConsecutive, roster.cntDays - dayCount + 1)
+
+            # Überprüfung: minimale consecutive Arbeitstage müssen auch gearbeitet werden
+            if (consecDays < consecutiveStart * minDays):
+                return False
+        dayCount += 1
+
+    return True
+
+
+def consecStart(roster, current_date, employee):
+    """
+    Gibt an, ob es sich bei Datum um start einer consecutiven Reihe an Arbeitstagen handelt
+    :param roster:
+    :return: 1, falls ja, 0 wenn nicht
+    """
+    startTime = datetime.strptime(roster.start, '%Y-%m-%d')
+    start_date = date(startTime.year, startTime.month, startTime.day)
+    # xstart berechnen (gibt an, ob start consecutiver reihe)
+    sdDay = current_date.strftime("%Y-%m-%d")
+
+    eWorks = getShiftByEmployeeByDate01(roster, sdDay, employee)
+    if (current_date == start_date):  # für ersten Tag in Periode
+        if (employee.history['lastAssignedShiftType'] != "Frueh" and employee.history[
+            'lastAssignedShiftType'] != "Spaet"):
+            eWorksY = 0
+        else:
+            eWorksY = 1
+    else:
+        sdDayY = current_date - timedelta(days=1)
+        sdDayY = sdDayY.strftime("%Y-%m-%d")
+        eWorksY = getShiftByEmployeeByDate01(roster, sdDayY, employee)
+
+    if (eWorks == 1 and eWorksY == 0):
+        return (1)
+    else:
+        return (0)
+
+
+def maxConsec(roster):
+    """
+    Constraint 11
+    :param roster:
+    :return:
+    """
+    currentTime = datetime.strptime(roster.start, '%Y-%m-%d')
+    start_date = date(currentTime.year, currentTime.month, currentTime.day)
+    end_period = currentTime + timedelta(days=roster.cntDays - 1)
+    end_date = date(end_period.year, end_period.month, end_period.day)
+
+    for i in roster.employees:
+        dayCount = 1
+        for single_date in daterange(start_date, end_date - timedelta(days=i.maxConsecutive + 1)):
+            daySum = dayCount + i.maxConsecutive
+            dayStart = dayCount + i.minConsecutive
+
+            # Summe
+            consecDaysSum = 0
+            for j in range(dayStart, daySum + 1):
+                d = single_date + timedelta(days=j - 1)
+                d = d.strftime("%Y-%m-%d")
+                consecDaysSum += (1 - getShiftByEmployeeByDate01(roster, d, i))
+
+            # xestart
+            consecutiveStart = consecStart(roster, single_date, i)
+
+            # Überprüfung
+            if (consecDaysSum < consecutiveStart):
+                return False
+
+            dayCount += 1
+    return True
+
+
+def minConsecPeriod(roster):
+    """
+    Constraint 18
+    :param roster:
+    :return:
+    """
+
+    currentTime = datetime.strptime(roster.start, '%Y-%m-%d')
+    start_date = date(currentTime.year, currentTime.month, currentTime.day)
+
+    for i in roster.employees:  # für alle Mitarbeiter
+        dayCount = 1
+        if (i.minConsecutive > i.history['numberOfConsecutiveWorkingDays']):
+
+            # linke Summe
+            daySum = min(i.minConsecutive - i.history['numberOfConsecutiveWorkingDays'], roster.cntDays)
+            consecDays = 0
+            for j in range(dayCount, daySum + 1):
+                d = start_date + timedelta(days=j - 1)
+                d = d.strftime("%Y-%m-%d")
+                consecDays += getShiftByEmployeeByDate01(roster, d, i)
+
+            # rechter Teil
+            eWorksY = 0
+            if (i.history['lastAssignedShiftType'] == "Frueh" or i.history['lastAssignedShiftType'] == "Spaet"):
+                eWorksY = 1
+            rSum = min(i.minConsecutive - i.history['numberOfConsecutiveWorkingDays'], roster.cntDays - dayCount + 1)
+
+            if (consecDays < eWorksY * rSum):
+                return False
+    return True
+
+
+def maxConsecPeriod(roster):
+    """
+    Constraint 19
+    :param roster:
+    :return:
+    """
+    currentTime = datetime.strptime(roster.start, '%Y-%m-%d')
+    start_date = date(currentTime.year, currentTime.month, currentTime.day)
+
+    for i in roster.employees:  # für alle Mitarbeiter
+        dayCount = 1
+        if (i.maxConsecutive - i.history['numberOfConsecutiveWorkingDays'] < roster.cntDays):
+
+            # linke Summe
+            daySum = dayCount + i.maxConsecutive - i.history['numberOfConsecutiveWorkingDays']
+            consecDays = 0
+            for j in range(dayCount, daySum + 1):
+                d = start_date + timedelta(days=j - 1)
+                d = d.strftime("%Y-%m-%d")
+                consecDays += (1 - getShiftByEmployeeByDate01(roster, d, i))
+
+            # rechter Teil
+            eWorksY = 0
+            if (i.history['lastAssignedShiftType'] == "Frueh" or i.history['lastAssignedShiftType'] == "Spaet"):
+                eWorksY = 1
+
+            if (consecDays < eWorksY):
+                return False
     return True
